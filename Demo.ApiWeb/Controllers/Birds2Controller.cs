6d751c4324edb0353d0cffaf5f4810953d4c47da
吳剛志 <chicken@chicken-house.net>
2017-01-18 01:25:07 +0800

    create /api/birds2 controller

---
+++ Demo.ApiWeb/Controllers/Birds2Controller.cs
@@ -0,0 +1,56 @@
+﻿using Demo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Demo.ApiWeb.Controllers
+{
+    [SDKVersionCheckActionFilter]
+    public class Birds2Controller : ApiController
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            BirdInfoRepo.Init(File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("~/App_Data/birds.json")));
+            this.APIVersion = this.GetType().Assembly.GetName().Version;
+            base.Initialize(controllerContext);
+        }
+
+        private Version APIVersion { get; set; }
+
+        /// <summary>
+        /// OPTION: 傳回 API Version
+        /// </summary>
+        /// <returns></returns>
+        public string Options()
+        {
+            return this.APIVersion.ToString();
+        }
+
+        /// <summary>
+        /// HEAD: 不傳回資料，只透過 HEADER 傳回資料總筆數，方便前端 APP 預知總比數，計算進度及分頁頁數。
+        /// </summary>
+        public void Head()
+        {
+            System.Web.HttpContext.Current.Response.AddHeader("X-DATAINFO-TOTAL", BirdInfoRepo.Data.Count().ToString());
+            return;
+        }
+
+
+        public string Get(string id)
+        {
+            return id;
+        }
+
+
+        public string Get()
+        {
+            return "1,2,3,4,5";
+        }
+
+    }
+}
